8e95343406c47e87b2418156e2947e8951c3fedf
Matt <60991158+matthewfee@users.noreply.github.com>
2023-09-30 09:11:43 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    plugins: [
+        'prettier-plugin-tailwindcss',
+    ],
+    printWidth: 80,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+}
+export default config
