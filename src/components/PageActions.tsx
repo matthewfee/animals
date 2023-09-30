b9beab9f93a910446a9752a2e5a2da7b39736d94
Matt <60991158+matthewfee@users.noreply.github.com>
2023-09-30 10:32:29 +0100

    --add: PageActions

---
+++ src/components/PageActions.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button.tsx'
+
+export const PageActions = () => {
+  return (
+    <div className={'flex w-full justify-end border border-white p-4'}>
+      <Button>Create New Animal</Button>
+    </div>
+  )
+}
