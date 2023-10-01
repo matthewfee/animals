70c8f772e6ec8ebf5c2ab9f1dd2eeab29df4718b
Matt <60991158+matthewfee@users.noreply.github.com>
2023-10-01 22:58:52 +0100

    --add: interval for random sounds

--- src/components/tables/animals/AnimalsTable.tsx
+++ src/components/tables/animals/AnimalsTable.tsx
@@ -1,9 +1,25 @@
 import { useAnimalsStore } from '@/store/animalsStore.ts'
 import { DataTable } from '@/components/tables/DataTable.tsx'
 import { ANIMAL_COLUMNS } from '@/components/tables/animals/AnimalColumns.tsx'
+import { Button } from '@/components/ui/button.tsx'
+import { useCallback, useEffect } from 'react'
+import { getRandom } from '@/utils/random.ts'
+import { useSound } from '@/hooks/useSound.ts'
 
 export const AnimalsTable = () => {
   const animals = useAnimalsStore((state) => state.animals)
+  const { makeSound } = useSound()
+  const makeRandomSound = useCallback(() => {
+    const randomAnimal = getRandom(animals)
+    makeSound(randomAnimal)
+  }, [animals, makeSound])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      makeRandomSound()
+    }, 10000)
+    return () => clearInterval(interval)
+  }, [makeRandomSound])
 
   return (
     <div className='container mx-auto py-10'>
